67d24cb4257dd14324c808004f927230571c664b
Mohammad Kevin <kvn4.200581@gmail.com>
2026-02-10 14:01:27 +0700

    cek

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -13,5 +13,6 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   }
   async onModuleInit() {
     await this.$connect();
+    console.log('Database connected:', process.env.DATABASE_URL);
   }
 }
