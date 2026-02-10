d2dfba1001035a973b2b5549adbda22f11df75c7
Mohammad Kevin <kvn4.200581@gmail.com>
2026-02-10 15:03:17 +0700

    fix all

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Library API')
@@ -24,7 +18,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(3000);
 }
-
 bootstrap();
